79f34331457ef249cb7ffef663fa7bda2e515a1a
EvodiaAut <evodia.aut@gmail.com>
2018-02-01 22:24:06 +0100

    add vue to moduleNameMapper

--- test/unit/jest.conf.js
+++ test/unit/jest.conf.js
@@ -8,7 +8,8 @@ module.exports = {
     'vue'
   ],
   moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/src/$1'
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '^vue$': 'vue/dist/vue.common.js'
   },
   transform: {
     '^.+\\.js$': '<rootDir>/node_modules/babel-jest',
