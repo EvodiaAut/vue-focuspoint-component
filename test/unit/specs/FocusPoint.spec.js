4b2082a1d3e5705fd211012f8704d4b2fca26dce
EvodiaAut <evodia.aut@gmail.com>
2018-02-01 22:32:39 +0100

    add unit test for FocusPoint

--- test/unit/specs/FocusPoint.spec.js
+++ test/unit/specs/FocusPoint.spec.js
@@ -1,9 +1,34 @@
 import { shallow } from 'vue-test-utils'
 import FocusPoint from '@/components/FocusPoint'
 
+const propsData = {
+  focus: {
+    x: 50,
+    y: 50
+  },
+  focusDefault: {
+    x: 50,
+    y: 50
+  },
+  decimalLength: 5
+}
+
 describe('FocusPoint.vue', () => {
   it('should render correct contents', () => {
-    const vm = shallow(FocusPoint)
-    expect(vm.isVueInstance()).toBe(true)
+    const wrapper = shallow(FocusPoint, { propsData })
+
+    expect(wrapper.isVueInstance()).toBe(true)
+    expect(wrapper.props().focus).toBe(propsData.focus)
+    expect(wrapper.props().focusDefault).toBe(propsData.focusDefault)
+    expect(wrapper.props().decimalLength).toBe(propsData.decimalLength)
+  })
+
+  it('should render correct pin', () => {
+    const wrapper = shallow(FocusPoint, { propsData })
+    const pin = wrapper.find('.focus-point-pin')
+
+    expect(pin.isVueInstance()).toBe(false)
+    expect(pin.is('div')).toBe(true)
+    expect(pin.classes()).toContain('focus-point-pin')
   })
 })
